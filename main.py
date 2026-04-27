07a88f3ce007023e1721483b56ba5402bbff850a
DongJin Kim <31959020+ywydpapa@users.noreply.github.com>
2026-04-28 01:01:30 +0900

    전체 손봄

--- main.py
+++ main.py
@@ -55,6 +55,17 @@ async def artofasso(request: Request):
         },
     )
 
+
+@app.get("/notices", response_class=HTMLResponse)
+async def notices(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/mainpages/notice.html",
+        context={
+            "title": "IYCF - 국제청소년문화재단",
+        },
+    )
+
 @app.get("/shingo", response_class=HTMLResponse)
 async def shingo(request: Request):
     return templates.TemplateResponse(
@@ -85,11 +96,193 @@ async def globl(request: Request):
         },
     )
 
+@app.get("/safe_school", response_class=HTMLResponse)
+async def safeschool(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/edu/sschool.html",
+        context={
+            "title": "IYCF - 국제청소년문화재단",
+        },
+    )
+
+
+@app.get("/equalis", response_class=HTMLResponse)
+async def equalis(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/edu/equalis.html",
+        context={
+            "title": "IYCF - 국제청소년문화재단",
+        },
+    )
+
 @app.get("/edu_main", response_class=HTMLResponse)
 async def edumain(request: Request):
     return templates.TemplateResponse(
         request=request,
         name="/edu/edu_main.html",
+        context={
+            "title": "IYCF - 국제청소년문화재단",
+        },
+    )
+
+
+@app.get("/eventmain", response_class=HTMLResponse)
+async def eventmain(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/events/eventmain.html",
+        context={
+            "title": "IYCF - 국제청소년문화재단",
+        },
+    )
+
+@app.get("/k_wellf", response_class=HTMLResponse)
+async def kwell(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/events/kwellf.html",
+        context={
+            "title": "IYCF - 국제청소년문화재단",
+        },
+    )
+
+
+@app.get("/galmer", response_class=HTMLResponse)
+async def galmer(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/events/galmer.html",
+        context={
+            "title": "IYCF - 국제청소년문화재단",
+        },
+    )
+
+
+@app.get("/2025_jagalchi", response_class=HTMLResponse)
+async def jagalchi25(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/events/jagalchi25.html",
+        context={
+            "title": "IYCF - 국제청소년문화재단",
+        },
+    )
+
+
+@app.get("/2025_yeongdo", response_class=HTMLResponse)
+async def yeongdo25(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/events/yeongdo25.html",
+        context={
+            "title": "IYCF - 국제청소년문화재단",
+        },
+    )
+
+@app.get("/chy_writer", response_class=HTMLResponse)
+async def chy_writer(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/events/chy_writer.html",
+        context={
+            "title": "IYCF - 국제청소년문화재단",
+        },
+    )
+
+
+@app.get("/safe_goldenbell", response_class=HTMLResponse)
+async def safe_goldenbell(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/events/safe_goldenbell.html",
+        context={
+            "title": "IYCF - 국제청소년문화재단",
+        },
+    )
+
+
+@app.get("/scholarship_delivery", response_class=HTMLResponse)
+async def scholarship_delivery(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/events/scholarship_delivery.html",
+        context={
+            "title": "IYCF - 국제청소년문화재단",
+        },
+    )
+
+
+@app.get("/youth_volunteer", response_class=HTMLResponse)
+async def youthv(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/events/youth_volunteer.html",
+        context={
+            "title": "IYCF - 국제청소년문화재단",
+        },
+    )
+
+
+@app.get("/reporter_notice", response_class=HTMLResponse)
+async def reporter_notice(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/events/reporter_notice.html",
+        context={
+            "title": "IYCF - 국제청소년문화재단",
+        },
+    )
+
+
+@app.get("/young_reporter", response_class=HTMLResponse)
+async def youngreporter(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/events/y_reporter.html",
+        context={
+            "title": "IYCF - 국제청소년문화재단",
+        },
+    )
+
+
+@app.get("/childrensafe", response_class=HTMLResponse)
+async def childrensafe(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/events/childrensafe.html",
+        context={
+            "title": "IYCF - 국제청소년문화재단",
+        },
+    )
+
+
+@app.get("/childrensafe2", response_class=HTMLResponse)
+async def childrensafe2(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/events/childrensafe2.html",
+        context={
+            "title": "IYCF - 국제청소년문화재단",
+        },
+    )
+
+@app.get("/env_education", response_class=HTMLResponse)
+async def envedu(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/events/env_education.html",
+        context={
+            "title": "IYCF - 국제청소년문화재단",
+        },
+    )
+
+@app.get("/elder_video", response_class=HTMLResponse)
+async def eldervideo(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/events/elder_video.html",
         context={
             "title": "IYCF - 국제청소년문화재단",
         },
